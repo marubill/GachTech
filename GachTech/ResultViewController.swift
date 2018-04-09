830bec5d75ead5b13ec747cb37c7b229532b62ca
中山弘瑛 <hiroaki1207.gk@gmail.com>
2018-04-09 12:37:57 +0900

    ガチャアプリ完成

--- GachTech/ResultViewController.swift
+++ GachTech/ResultViewController.swift
@@ -17,10 +17,10 @@ class ResultViewController: UIViewController {
     
     //モンスター画像を保存ておく配列
     var monsterArray: [UIImage]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         //0〜９の間でランダムに数字を発生させる
         let number = Int(arc4random_uniform(10))
@@ -48,25 +48,54 @@ class ResultViewController: UIViewController {
             haikeiImageView.image = UIImage (named: "bg_blue.png")
         }
         
-    
-    
-
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    
+    
+    //アニメーション/の中でもtransform（変形）させるキーパスを設定
+    let animation = CABasicAnimation(keyPath: "transform")
+    
+    //アニメーションの開始時の数値
+    animation.fromValue = NSNumber(value: 0)
+    
+    //アニメーション終了時の数値
+    animation.toValue = NSNumber(value: 2 * Double.pi)
+    
+    //Z軸を中心としたアニメーションにする
+    animation.valueFunction = CAValueFunction(name: kCAValueFunctionRotateZ)
+    
+    //アニメーションの速さを指定する
+    animation.duration = 5
+    
+    //アニメーションを何回繰り返すかを決定する（今回の場合は無限に繰り返す）
+    animation.repeatCount = Float.infinity
+    
+    //どのレイヤーをアニメーションさせるか指定する
+    haikeiImageView.layer.add(animation, forKey: nil)
+    
+    }
+    
+    @IBAction func modoru(){
+        self.dismiss(animated: true,completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
